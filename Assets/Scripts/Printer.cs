331b98fb786b0a9dcaa03464b0f589b61be17e29
enze-l <leon.enzenberger@protonmail.com>
2022-06-25 14:35:54 +0200

    Use Native Android code to make Popup

--- Assets/Scripts/Printer.cs
+++ Assets/Scripts/Printer.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Printer : MonoBehaviour
 {
-    private string text;
+    private bool isClicked = false;
+
+    private bool isFocus = false;
+
+    private string screenShotName;
+    private bool isProcessing = false;
+    private string shareSubject, shareMessage;
+
+    public string toastText = "This is a Toast";
+
     public void Print(){
-        text = "Button pressed";
+        isClicked = true;
+
+        screenShotName =  "qr-code.png";
+
+        shareSubject = "Print QR-Code";
+        shareMessage = "Print QR-Code";
+
+        ShareQRCode();
+    }
+
+    private void ShareQRCode(){
+         //create a Toast class object
+        AndroidJavaClass toastClass =
+                    new AndroidJavaClass("android.widget.Toast");
+
+        //create an array and add params to be passed
+        object[] toastParams = new object[3];
+        AndroidJavaClass unityActivity =
+          new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        toastParams[0] =
+                     unityActivity.GetStatic<AndroidJavaObject>
+                               ("currentActivity");
+        toastParams[1] = toastText;
+        toastParams[2] = toastClass.GetStatic<int>
+                               ("LENGTH_LONG");
+
+        //call static function of Toast class, makeText
+        AndroidJavaObject toastObject =
+                        toastClass.CallStatic<AndroidJavaObject>
+                                      ("makeText", toastParams);
+
+        //show toast
+        toastObject.Call("show");
+    }
+
+    void OnApplicationFocus(bool focus){
+        isFocus = focus;
     }
 
     void OnGUI()
     {
         GUIStyle guiStyle = new GUIStyle();
         guiStyle.fontSize = 50;
-        GUILayout.Label(" Hello " + text, guiStyle);
+        GUILayout.Label(" Clicked: " + isClicked, guiStyle);
     }
 }
