a9f123fc4a671c471ebaa461b4aa5f09d2d898ad
enze-l <leon.enzenberger@protonmail.com>
2022-06-28 14:33:36 +0200

    Make code production ready

--- Assets/Scripts/Printer.cs
+++ Assets/Scripts/Printer.cs
@@ -6,35 +6,26 @@ public class Printer : MonoBehaviour
 {
     private QREncoder qrEncoder;
 
-    private bool isClicked = false;
-    private string testText = "This is a Text";
-    private Texture2D qrCode;
-    private string qrFileName = "qrcode.png";
-    private string log;
-    private string qrFilePath;
-
     private void Start(){
         qrEncoder = new QREncoder();
     }
 
-    public void Print(){
-        isClicked = true;
-
-        CreateQRCode();
-        ShareQRCode();
+    public void Print(string text){
+        string qrFilePath = CreateQRCode(text);
+        ShareQRCode(qrFilePath);
     }
 
-    private void CreateQRCode(){
-        qrCode = qrEncoder.Encode(testText);
-        qrFilePath = Application.persistentDataPath + "/" + qrFileName;
+    private string CreateQRCode(string text){
+        Texture2D qrCode = qrEncoder.Encode(text);
+        string qrFilePath = Application.persistentDataPath + "/qrcode.png";
         byte[] bytes = qrCode.EncodeToPNG();
         System.IO.File.WriteAllBytes(qrFilePath, bytes);
-        log = bytes.Length/1024 + " saved to " + qrFilePath;
+        return qrFilePath;
     }
 
-    private void ShareQRCode(){
+    private void ShareQRCode(string qrFilePath){
         AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
 
         AndroidJavaClass photoPrinterClass = new AndroidJavaClass("androidx.print.PrintHelper");
         AndroidJavaObject photoPrinter = new AndroidJavaObject("androidx.print.PrintHelper", currentActivity);
@@ -45,15 +36,14 @@ public class Printer : MonoBehaviour
         AndroidJavaObject bitmap = bitmapFactory.CallStatic<AndroidJavaObject>("decodeFile", qrFilePath);
 
         photoPrinter.Call("printBitmap", "Print QR Code", bitmap);
-        log = "should have been printed";
     }
-    
+
 
     void OnGUI()
     {
         GUIStyle guiStyle = new GUIStyle();
         guiStyle.fontSize = 50;
-        GUILayout.Label(" Clicked: " + isClicked + " - " + log , guiStyle);
-        GUI.Button(new Rect(400, 400, 256, 256), qrCode, GUIStyle.none);
+        //GUILayout.Label(" Clicked: " + isClicked + " - " + log , guiStyle);
+        //GUI.Button(new Rect(400, 400, 256, 256), qrCode, GUIStyle.none);
     }
 }
